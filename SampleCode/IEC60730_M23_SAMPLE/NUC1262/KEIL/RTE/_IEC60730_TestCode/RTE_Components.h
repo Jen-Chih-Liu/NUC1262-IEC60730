6efd831e57ede754172564da9c81a48e357d9976
MB20 JCLiu <JCLiu@nuvoton.com>
2023-10-31 10:12:38 +0800

    first commit

---
+++ SampleCode/IEC60730_M23_SAMPLE/NUC1262/KEIL/RTE/_IEC60730_TestCode/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'IEC60730_TestCode' 
+ * Target:  'IEC60730_TestCode' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "NUC1262.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
